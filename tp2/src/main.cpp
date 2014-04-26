6862bd2b195c939cccdeb9e66886b77d52a420c5
christian <christian.russo8@gmail.com>
2014-04-25 22:46:20 -0300

    Clase Matrix

---
+++ tp2/src/main.cpp
@@ -0,0 +1,19 @@
+#define _USE_MATH_DEFINES
+#include <fstream>
+#include <istream>
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+#include "Matrix.h"
+
+using namespace std;
+
+
+// Main Release
+int main(int argc, char* argv[]) {
+    Matrix R(5, 1, 0.0);
+    cout << R;
+
+    return 0;
+}
