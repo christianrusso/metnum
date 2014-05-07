a90e4a3d297026532f5f8ea513173360e1f98c0b
christian <christian.russo8@gmail.com>
2014-05-07 00:37:23 -0300

    seteo a mano cantidad de personas

--- tp2/src/casesbuilder.py
+++ tp2/src/casesbuilder.py
@@ -2,6 +2,8 @@
 import math
 import random
 
+cantPersonasA = [5,15,25,30,41]
+
 directoryVariandoPersonas = 'cases/ImagenesCaras/variandoPersonas/'
 directoryVariandoPersonasEIters = 'cases/ImagenesCaras/variandoPersonasEIters/'
 directoryVariandoPersonasEItersECantImgs = 'cases/ImagenesCaras/variandoPersonasEItersECantImgs/'
@@ -13,26 +15,20 @@ directoryVariandoPersonasEItersRed = 'cases/ImagenesCarasRed/variandoPersonasEIt
 directoryVariandoPersonasEItersECantImgsRed = 'cases/ImagenesCarasRed/variandoPersonasEItersECantImgs/'
 directoryVariandoPersonasEItersECantImgsRandomRed = 'cases/ImagenesCarasRed/variandoPersonasEItersECantImgsRandom/'
 
-# Considerar al menos dos instancias de prueba, generando distintas discretizaciones para cada
-#una de ellas y comparando la ubicacion de la isoterma buscada respecto de la pared externa del
-#horno. Se sugiere presentar graficos de temperatura o curvas de nivel para los mismos, ya sea
-#utilizando las herramientas provistas por la catedra o implementando sus propias herramientas de
-#graficacion. Estudiar la proximidad de la isoterma buscada respecto de la pared exterior del horno
-#en funcion de distintas granularidades de discretizacion y las condiciones de borde. Analizar
-#tambien el tiempo de computo requerido en funcion de la granularidad de la discretizacion.
-
-
 def caseVariandoPersonas():
 	
 	description = 'Vario la cantidad de personas con 5 imagenes cada una con 5 imagenes y 15 iteraciones'
+	
 	for x in range(0,5):
 		personas = []
 		filename = 'case1'+chr(ord('a')+x)+'.in'	
 		f = open(directoryVariandoPersonas+filename,'w')
-		cantPersonas = random.randint(1, 41)
+		cantPersonas = cantPersonasA[x]
 		f.write("../data/ImagenesCaras/ 112 92 " + str(cantPersonas) + ' 5 15' +'\n')
+		#for x in range(0,cantPersonas):
+		#	personas.append(random.randint(0, 41))
 		for x in range(0,cantPersonas):
-			personas.append(random.randint(0, 41))
+			personas.append(x)
 		personas.sort()
 		for x in personas:
 			f.write("s" + str(x) + "/ 1 2 3 4 5" +'\n')
@@ -47,7 +43,7 @@ def caseVariandoPersonasEIters(maxIteraciones):
 		personas = []
 		filename = 'case1'+chr(ord('a')+x)+'.in'	
 		f = open(directoryVariandoPersonasEIters+filename,'w')
-		cantPersonas = random.randint(1, 41)
+		cantPersonas = cantPersonasA[x]
 		iters = random.randint(1,maxIteraciones)
 		f.write("../data/ImagenesCaras/ 112 92 " + str(cantPersonas) + ' 5 '+ str(iters) +'\n')
 		for x in range(0,cantPersonas):
@@ -66,7 +62,7 @@ def caseVariandoPersonasEItersECantImgs(maxIteraciones):
 		personas = []
 		filename = 'case1'+chr(ord('a')+x)+'.in'	
 		f = open(directoryVariandoPersonasEItersECantImgs+filename,'w')
-		cantPersonas = random.randint(1, 41)
+		cantPersonas = cantPersonasA[x]
 		iters = random.randint(1,maxIteraciones)
 		f.write("../data/ImagenesCaras/ 112 92 " + str(cantPersonas) + ' 5 '+ str(iters) +'\n')
 		for x in range(0,cantPersonas):
@@ -90,7 +86,7 @@ def caseVariandoPersonasEItersECantImgsRandom(maxIteraciones):
 		personas = []
 		filename = 'case1'+chr(ord('a')+x)+'.in'	
 		f = open(directoryVariandoPersonasEItersECantImgsRandom+filename,'w')
-		cantPersonas = random.randint(1, 41)
+		cantPersonas = cantPersonasA[x]
 		iters = random.randint(1,maxIteraciones)
 		f.write("../data/ImagenesCaras/ 112 92 " + str(cantPersonas) + ' 5 '+ str(iters) +'\n')
 		for x in range(0,cantPersonas):
@@ -122,7 +118,7 @@ def caseVariandoPersonasRed():
 		personas = []
 		filename = 'case1'+chr(ord('a')+x)+'.in'	
 		f = open(directoryVariandoPersonasRed+filename,'w')
-		cantPersonas = random.randint(1, 41)
+		cantPersonas = cantPersonasA[x]
 		f.write("../data/ImagenesCarasRed/ 28 23 " + str(cantPersonas) + ' 5 15' +'\n')
 		for x in range(0,cantPersonas):
 			personas.append(random.randint(0, 41))
@@ -140,7 +136,7 @@ def caseVariandoPersonasEItersRed(maxIteraciones):
 		personas = []
 		filename = 'case1'+chr(ord('a')+x)+'.in'	
 		f = open(directoryVariandoPersonasEItersRed+filename,'w')
-		cantPersonas = random.randint(1, 41)
+		cantPersonas = cantPersonasA[x]
 		iters = random.randint(1,maxIteraciones)
 		f.write("../data/ImagenesCarasRed/ 28 23 " + str(cantPersonas) + ' 5 '+ str(iters) +'\n')
 		for x in range(0,cantPersonas):
@@ -159,7 +155,7 @@ def caseVariandoPersonasEItersECantImgsRed(maxIteraciones):
 		personas = []
 		filename = 'case1'+chr(ord('a')+x)+'.in'	
 		f = open(directoryVariandoPersonasEItersECantImgsRed+filename,'w')
-		cantPersonas = random.randint(1, 41)
+		cantPersonas = cantPersonasA[x]
 		iters = random.randint(1,maxIteraciones)
 		f.write("../data/ImagenesCarasRed/ 28 23 " + str(cantPersonas) + ' 5 '+ str(iters) +'\n')
 		for x in range(0,cantPersonas):
@@ -183,7 +179,7 @@ def caseVariandoPersonasEItersECantImgsRandomRed(maxIteraciones):
 		personas = []
 		filename = 'case1'+chr(ord('a')+x)+'.in'	
 		f = open(directoryVariandoPersonasEItersECantImgsRandomRed+filename,'w')
-		cantPersonas = random.randint(1, 41)
+		cantPersonas = cantPersonasA[x]
 		iters = random.randint(1,maxIteraciones)
 		f.write("../data/ImagenesCarasRed/ 28 23 " + str(cantPersonas) + ' 5 '+ str(iters) +'\n')
 		for x in range(0,cantPersonas):
@@ -203,13 +199,14 @@ def caseVariandoPersonasEItersECantImgsRandomRed(maxIteraciones):
 
 
 caseVariandoPersonas(); 
-caseVariandoPersonasEIters(100);
-caseVariandoPersonasEItersECantImgs(100);
-caseVariandoPersonasEItersECantImgsRandom(100);
+#caseVariandoPersonasEIters(100);
+#caseVariandoPersonasEItersECantImgs(100);
+#caseVariandoPersonasEItersECantImgsRandom(100);
 
 
-caseVariandoPersonasRed(); 
-caseVariandoPersonasEItersRed(100);
-caseVariandoPersonasEItersECantImgsRed(100);
-caseVariandoPersonasEItersECantImgsRandomRed(100);
+
+#caseVariandoPersonasRed(); 
+#caseVariandoPersonasEItersRed(100);
+#caseVariandoPersonasEItersECantImgsRed(100);
+#caseVariandoPersonasEItersECantImgsRandomRed(100);
 
