bb4e5efdde4e07fd80eb4ef02b0bc98fad53d63b
LucianoGandini <gl.gandini@gmail.com>
2014-06-05 19:23:48 -0300

    agregando otro visualizador para nuestros tests

---
+++ tp3/src/runTests.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from subprocess import *
+from glob import glob
+import os
+from fnmatch import fnmatch
+
+
+# Programa compilado
+executable = './yoAtajo' if os.name == 'posix' else 'yoAtajo.exe'
+
+def listfiles(root, pattern):
+  for path, subdirs, files in os.walk(root):
+    for name in files:
+      if fnmatch(name, pattern):
+        yield os.path.join(path, name)
+
+
+def runTp(input, output):
+    """Invoca al tp con input y output como parametros"""
+    #fileOut = open(input.replace(".in", ".console"), 'w')
+    print("usando metodo 1")
+    call([executable, input, output, '1'] , stdin=None, stdout=PIPE, stderr=PIPE)
+    #if (input.find("Met0") > 0):
+    #	print("usando metodo 0")
+    #	call([executable, input, output, '0'] , stdin=None, stdout=fileOut, stderr=fileOut)
+    #else:
+    #	print("usando metodo 0")
+    #	call([executable, input, output, '0'] , stdin=None, stdout=fileOut, stderr=fileOut)
+
+def run():
+	for fname in listfiles('../testVisualizador/instancias', '*.tiro'):
+		#if (fname.replace(".tiro", ".out.times") in listfiles('cases', '*.times')):
+		#	print("Case " + fname + " was already run")
+		#else:
+		#	print("Running case: " + fname)
+		runTp(fname,  fname.replace(".tiro", ".arq"))
+
+run();
