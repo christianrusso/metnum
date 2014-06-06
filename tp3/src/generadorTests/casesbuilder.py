2e21d38f50d735c2805e6ecb7cdb2745e6596df6
adanos <adanos@adanos-desktop.(none)>
2014-06-06 12:51:07 -0300

    Agregados comentarios al caso lineal

--- tp3/src/generadorTests/casesbuilder.py
+++ tp3/src/generadorTests/casesbuilder.py
@@ -9,10 +9,13 @@ def caseLineal(palo1, palo2,bx,by):
 	for i in range(1,11):
 		filename = 'lineal'+str(i)+'.tiro'
 		f = open(directory+filename,'w')
+        # (bx,by) es) la posicion inicial de la pelota.
+        # (ax,ay) describen la linealidad del eje x segun el tiempo
+        # (x0,y0) es la posicion por donde va a entrar la pelota al arco.
 		bx = random.uniform(300,1000)
-		by = random.uniform(0,500)	
+		by = random.uniform(0,500)
 		f.write("375 315 385 4"  +'\n')
-		ax = 1.0		
+		ax = 1.0
 		ay = 1.0
 		x0 = 100.0
 		y0 = random.uniform(palo1, palo2)
@@ -33,7 +36,7 @@ def caseLineal(palo1, palo2,bx,by):
 
 
 def linealFunction(palo1,palo2,bx,by):
-	ax = 1.0		
+	ax = 1.0
 	ay = 1.0
 	x0 = 100.0
 	y0 = 300.0
@@ -41,12 +44,12 @@ def linealFunction(palo1,palo2,bx,by):
 	y1 = by
 	my = -(y1-y0)/(x1-x0)
 	result_x = 1000.0
-	t = 0 
+	t = 0
 	while(result_x > 100.0):
 		result_y = my*t +by
 		result_x = -ax*t + bx
 		print str(result_x) + " " + str(result_y);
 		t = t + 1
-	
+
 #caseLineal();
 caseLineal(375,315,300,200);
