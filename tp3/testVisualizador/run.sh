071140cbc419c5e5120b11496fdc2e5587266162
VirtualBoxImages.com <adminuser@adminuser-VirtualBox.(none)>
2014-06-10 22:10:18 -0400

    mas instancias

--- tp3/testVisualizador/run.sh
+++ tp3/testVisualizador/run.sh
@@ -36,8 +36,39 @@
 
 
 # exoticos
-python main.py instancias/exoticos/ex1.arq instancias/exoticos/ex1.tiro instancias/exoticos/ex1.jug
-python main.py instancias/exoticos/ex2.arq instancias/exoticos/ex2.tiro instancias/exoticos/ex2.jug
-python main.py instancias/exoticos/ex3.arq instancias/exoticos/ex3.tiro instancias/exoticos/ex3.jug
-python main.py instancias/exoticos/ex4.arq instancias/exoticos/ex4.tiro instancias/exoticos/ex4.jug
-python main.py instancias/exoticos/ex5.arq instancias/exoticos/ex5.tiro instancias/exoticos/ex5.jug
+#python main.py instancias/exoticos/ex1.arq instancias/exoticos/ex1.tiro instancias/exoticos/ex1.jug
+#python main.py instancias/exoticos/ex2.arq instancias/exoticos/ex2.tiro instancias/exoticos/ex2.jug
+#python main.py instancias/exoticos/ex3.arq instancias/exoticos/ex3.tiro instancias/exoticos/ex3.jug
+#python main.py instancias/exoticos/ex4.arq instancias/exoticos/ex4.tiro instancias/exoticos/ex4.jug
+#python main.py instancias/exoticos/ex5.arq instancias/exoticos/ex5.tiro instancias/exoticos/ex5.jug
+
+
+#de la catedra
+
+#python main.py instancias/instancias3/1.arq instancias/instancias3/1.tiro instancias/instancias3/1.jug
+#python main.py instancias/instancias3/2.arq instancias/instancias3/2.tiro instancias/instancias3/2.jug
+#python main.py instancias/instancias3/3.arq instancias/instancias3/3.tiro instancias/instancias3/3.jug
+#python main.py instancias/instancias3/4.arq instancias/instancias3/4.tiro instancias/instancias3/4.jug
+
+
+#python main.py instancias/instancias2/cbp30.arq instancias/instancias2/cbp30.tiro instancias/instancias2/cbp30.jug
+#python main.py instancias/instancias2/cbp70.arq instancias/instancias2/cbp70.tiro instancias/instancias2/cbp70.jug
+#python main.py instancias/instancias2/cp30.arq instancias/instancias2/cp30.tiro instancias/instancias2/cp30.jug
+#python main.py instancias/instancias2/cp70.arq instancias/instancias2/cp70.tiro instancias/instancias2/cp70.jug
+#python main.py instancias/instancias2/f2p90.arq instancias/instancias2/f2p90.tiro instancias/instancias2/f2p90.jug
+#python main.py instancias/instancias2/fp30.arq instancias/instancias2/fp30.tiro instancias/instancias2/fp30.jug
+#python main.py instancias/instancias2/fp70.arq instancias/instancias2/fp70.tiro instancias/instancias2/fp70.jug
+#python main.py instancias/instancias2/rp30.arq instancias/instancias2/rp30.tiro instancias/instancias2/rp30.jug
+#python main.py instancias/instancias2/rp70.arq instancias/instancias2/rp70.tiro instancias/instancias2/rp70.jug
+
+
+#con ruido
+python main.py instancias/instancias2/cbp30.arq instancias/instancias2/cbp30.tiroruidoso instancias/instancias2/cbp30.jug
+python main.py instancias/instancias2/cbp70.arq instancias/instancias2/cbp70.tiroruidoso instancias/instancias2/cbp70.jug
+python main.py instancias/instancias2/cp30.arq instancias/instancias2/cp30.tiroruidoso instancias/instancias2/cp30.jug
+python main.py instancias/instancias2/cp70.arq instancias/instancias2/cp70.tiroruidoso instancias/instancias2/cp70.jug
+python main.py instancias/instancias2/f2p90.arq instancias/instancias2/f2p90.tiroruidoso instancias/instancias2/f2p90.jug
+python main.py instancias/instancias2/fp30.arq instancias/instancias2/fp30.tiroruidoso instancias/instancias2/fp30.jug
+python main.py instancias/instancias2/fp70.arq instancias/instancias2/fp70.tiroruidoso instancias/instancias2/fp70.jug
+python main.py instancias/instancias2/rp30.arq instancias/instancias2/rp30.tiroruidoso instancias/instancias2/rp30.jug
+python main.py instancias/instancias2/rp70.arq instancias/instancias2/rp70.tiroruidoso instancias/instancias2/rp70.jug
