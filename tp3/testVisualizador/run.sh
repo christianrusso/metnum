1631a2838a7a7b691d5401fd80ea939e53c3687e
VirtualBoxImages.com <adminuser@adminuser-VirtualBox.(none)>
2014-06-11 20:46:41 -0400

    instancias

--- tp3/testVisualizador/run.sh
+++ tp3/testVisualizador/run.sh
@@ -1,12 +1,21 @@
 # rectas
-python main.py instancias/rectas/r1.arq instancias/rectas/r1.tiro instancias/vacio.jug
+python main.py instancias/rectas/lineal1.arq instancias/rectas/lineal1.tiro instancias/vacio.jug
+python main.py instancias/rectas/lineal2.arq instancias/rectas/lineal2.tiro instancias/vacio.jug
+python main.py instancias/rectas/lineal3.arq instancias/rectas/lineal3.tiro instancias/vacio.jug
+python main.py instancias/rectas/lineal4.arq instancias/rectas/lineal4.tiro instancias/vacio.jug
+python main.py instancias/rectas/lineal5.arq instancias/rectas/lineal5.tiro instancias/vacio.jug
+python main.py instancias/rectas/lineal6.arq instancias/rectas/lineal6.tiro instancias/vacio.jug
+python main.py instancias/rectas/lineal7.arq instancias/rectas/lineal7.tiro instancias/vacio.jug
+python main.py instancias/rectas/lineal8.arq instancias/rectas/lineal8.tiro instancias/vacio.jug
+python main.py instancias/rectas/lineal9.arq instancias/rectas/lineal9.tiro instancias/vacio.jug
+python main.py instancias/rectas/lineal10.arq instancias/rectas/lineal10.tiro instancias/vacio.jug
 # python main.py instancias/rectas/r2.arq instancias/rectas/r2.tiro instancias/vacio.jug
 # python main.py instancias/rectas/r3.arq instancias/rectas/r3.tiro instancias/vacio.jug
 # python main.py instancias/rectas/r4.arq instancias/rectas/r4.tiro instancias/vacio.jug
 # python main.py instancias/rectas/r5.arq instancias/rectas/r5.tiro instancias/vacio.jug
 
 # cuadraticas
-python main.py instancias/cuadraticas/c1.arq instancias/cuadraticas/c1.tiro instancias/vacio.jug
+#python main.py instancias/cuadraticas/c1.arq instancias/cuadraticas/c1.tiro instancias/vacio.jug
 # python main.py instancias/cuadraticas/c2.arq instancias/cuadraticas/c2.tiro instancias/vacio.jug
 # python main.py instancias/cuadraticas/c3.arq instancias/cuadraticas/c3.tiro instancias/vacio.jug
 # python main.py instancias/cuadraticas/c4.arq instancias/cuadraticas/c4.tiro instancias/vacio.jug
@@ -20,4 +29,4 @@ python main.py instancias/cuadraticas/c1.arq instancias/cuadraticas/c1.tiro inst
 # python main.py instancias/cubicas/cb5.arq instancias/cubicas/cb5.tiro instancias/vacio.jug
 
 # fancy
-python main.py instancias/fancy/tirolibre1.arq instancias/fancy/tirolibre1.tiro instancias/fancy/tirolibre1.jug
+#python main.py instancias/fancy/tirolibre1.arq instancias/fancy/tirolibre1.tiro instancias/fancy/tirolibre1.jug
